d4574038db0bf26358114bf7b94127657ec672e3
isaki0317 <matsuoisaki.0317@gmail.com>
2021-01-26 12:32:48 +0000

    [update]3章4-1完了

---
+++ practice_4.sql
@@ -0,0 +1,41 @@
+-- メモ
+--- DISTINCT(ディスティンクト)：重複業を除外(SELECTの直後に記述)
+--- ORDER BY：検索結果の順序並び替え
+--- OFFSET-FETCH：検索結果から件数を限定して取得
+----- 集合演算子 ------
+--- UNION：検索結果に他の検索結果を足し合わせる
+--- EXCEPT：検索結果から他の検索結果を差し引く
+--- INTERSECT：検索結果と他の検索結果で重複する部分を取得する
+
+-- 4章演習問題
+
+--.1-1
+SELECT * FROM 注文履歴
+ORDER BY 注文番号, 注文枝番
+
+--.1-2
+SELECT DISTINCT 商品名 FROM 注文履歴
+WHERE 日付 >= '2018-01-01' AND 日付 <= '2020-01-31'
+ORDER BY 商品名
+
+--.1-3
+SELECT 注文番号, 注文枝番, 注文金額 FROM 注文履歴
+WHERE 分類 = '1'
+ORDER BY 注文金額
+OFFSET 1 ROWS FETCH NEXT 3 NEXT ONLY
+
+--.1-4
+SELECT 日付, 商品名, 単価, 数量, 注文金額 FROM 注文履歴
+WHERE 分類 = '3', 数量 >= 2
+ORDER BY 日付, 数量 DESC
+
+--.1-5
+SELECT DISTINCT 分類, 商品名, サイズ, 単価 FROM 注文履歴
+WHERE 分類 = '1'
+UNION
+SELECT DISTINCT 分類, 商品名, 単価 FROM 注文履歴
+WHERE 分類 = '2'
+UNION
+SELECT DISTINCT 分類, 商品名, 単価 FROM 注文履歴
+WHERE 分類 = '3'
+ORDER BY 分類, 商品名
