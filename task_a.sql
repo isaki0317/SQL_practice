fe46538fdaaf7aa76c61355b99dbc6799c188552
isaki0317 <matsuoisaki.0317@gmail.com>
2021-01-30 04:08:51 +0000

    [update]Lv4

--- task_a.sql
+++ task_a.sql
@@ -87,3 +87,49 @@ SELECT 口座番号, 名義, '×' AS 口座区分 FROM 廃止口座 ORDER BY 名
 --LEVEL4
 
 --34
+SELECT 口座番号, 残高 / 1000 AS 千円単位の残高 FROM 口座 WHERE 残高 >= 1000000
+--35
+INSERT INTO 口座 (口座番号, 名義, 種別, 残高, 更新日)
+VALUES('06565', 'タカギ　ノブオ', '1', 10000 + 3000, '2018-04-01')
+--36
+UPDATE 口座 SET 残高 = (残高 - 3000) * 1.003
+WHERE 口座番号 IN ('06565', '1026413')
+--37
+SELECT 口座番号, 更新日, 更新日 + 180 AS 通帳期限日
+FROM 口座 WHERE 更新日 < '2017-01-01'
+--38
+SELECT 口座番号, 'カ)' || 名義 AS 名義 FROM 口座 WHERE 種別 = '3'
+--39
+SELECT DISTINCT 種別 AS 種別コード CASE 種別 WHEN '1' THEN '普通'
+WHEN '2' THEN '当座' WHEN '3' THEN '別段' END AS 種別名 FROM 口座
+--40
+SELECT 口座番号, 名義 CASE 残高 WHEN 残高 < 100000 THEN 'C'
+WHEN 残高 >= 100000 AND 残高 < 1000000 THEN 'B'
+ELSE 'A' END AS 残高ランク FROM 口座
+--41
+SELECT LENGTH(口座番号), LENGTH(REPLACE(名義, '　', '')),
+LENGTH(CAST(残高 AS VARCHER)) FROM 口座
+--42
+SELECT * FROM 口座 WHERE SUBSTRING(名義, 1, 5) LIKE '%カワ%'
+--43
+--44
+SELECT 口座番号, 残高, TRUNC(残高 * 0.0002, 0) AS 利息 FROM 口座 ORDER BY 残高 DESC
+--45
+SELECT 口座番号, 残高, CASE 残高 WHEN 残高 < 500000 THEN TRUNC(残高 * 0.0001, 0)
+WHEN 残高 >= 500000 AND 残高 < 2000000 THEN TRUNC(残高 * 0.0002, 0)
+ELSE TRUNC(残高 * 0.0003, 0) END AS 残高別利息 FROM 口座
+ORDER BY 残高別利息 DESC, 口座番号
+--46
+INSERT INTO 口座 (口座番号, 名義, 種別, 残高, 更新日)
+VALUES('0351262', 'イトウ　ダイ', '2', 635110, CURRENT_DATE)
+--47
+SELECT 口座番号, 名義, 種別, 残高, SUBSTRING(CAST(更新日 AS VARCHER), 1, 4) ||
+'年' || SUBSTRING(CAST(更新日 AS VARCHER), 6, 2) || '月' ||
+SUBSTRING(CAST(更新日 AS VARCHER), 9, 2) || '日' AS 更新日
+FROM 口座 WHERE 更新日 >= '2018-01-01'
+--48
+SELECT COALESCE(CAST(更新日 AS VARCHER), '設定なし') AS 更新日 FROM 口座
+
+--LEVEL5
+
+--49
