8c526a438a6e947f64d679a494a6370d951a45b3
isaki0317 <matsuoisaki.0317@gmail.com>
2021-01-30 05:41:21 +0000

    [update]Lv.5

--- task_a.sql
+++ task_a.sql
@@ -133,3 +133,33 @@ SELECT COALESCE(CAST(更新日 AS VARCHER), '設定なし') AS 更新日 FROM �
 --LEVEL5
 
 --49
+SELECT SUM(残高) AS 合計, MAX(残高) AS 最高, MIN(残高) AS 最小, 
+AVG(残高) AS 平均, COUNT(*) AS 件数 FROM 口座
+--50
+SELECT COUNT(*) AS 件数 FROM 口座 
+WHERE 種別 <> '1' AND 残高 >= 1000000 AND 更新日 < '2018-01-01'
+--51
+SELECT COUNT(*) - COUNT(更新日) AS 件数 FROM 口座
+--52
+SELECT MAX(名義) AS 最大値, MIN(名義) AS 最小値 FROM 口座
+--53
+SELECT MAX(更新日) AS 最大, MIN(更新日) AS 最小 FROM 口座
+--54
+SELECT 種別, SUM(残高) AS 合計, MAX(残高) AS 最高, MIN(残高) AS 最小, 
+AVG(残高) AS 平均, COUNT(*) AS 件数 FROM 口座 GROUP BY 種別
+--55
+SELECT SUBSTRING(口座番号, 7, 1) AS グループ, COUNT(*) AS 件数 FROM 口座
+GROUP BY SUBSTRING(口座番号, 7, 1) ORDER BY 件数 DESC
+--56
+SELECT SUBSTRING(COALESCE(CAST(更新日 AS VARCHER),'XXXX'), 1, 4) AS 更新年
+SUM(残高) AS 合計, MAX(残高) AS 最高, MIN(残高) AS 最小, 
+AVG(残高) AS 平均, COUNT(*) AS 件数 FROM 口座
+GROUP BY SUBSTRING(COALESCE(CAST(更新日 AS VARCHER),'XXXX'), 1, 4)
+--57
+SELECT 種別, SUM(残高) AS 合計, COUNT(*) AS 件数 FROM　口座
+GROUP BY 種別 HAVING SUM(残高) > 3000000
+--58
+
+--LEXEL6
+
+--59
